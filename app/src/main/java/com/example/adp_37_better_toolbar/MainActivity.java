e532a72d0788678fd7352bfd257cd52d2e96608d
montazeri_a <montazeri_a@office.zharfpouyan.net>
2022-03-28 10:13:54 +0330

    =new

---
+++ app/src/main/java/com/example/adp_37_better_toolbar/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.adp_37_better_toolbar;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == R.id.share) {
+            Toast.makeText(getApplicationContext(), "You click Share", Toast.LENGTH_SHORT).show();
+        } else if(id == R.id.about) {
+            Toast.makeText(getApplicationContext(), "You click About", Toast.LENGTH_SHORT).show();
+        } else if(id == R.id.exit) {
+            Toast.makeText(getApplicationContext(), "You click Exit", Toast.LENGTH_SHORT).show();
+        } else if(id == R.id.search) {
+            Toast.makeText(getApplicationContext(), "You click Search", Toast.LENGTH_SHORT).show();
+        } else if(id == R.id.setting) {
+            Toast.makeText(getApplicationContext(), "You click Setting", Toast.LENGTH_SHORT).show();
+        }
+
+        return  true;
+    }
+}
